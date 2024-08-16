080176939bb1983e102709dde9b91cc882bf2c05
Student9@ideaportriga.com <Student9@ideaportriga.com>
2024-08-16 12:58:52 +0300

    #23 Movie Poster Preview

---
+++ force-app/main/default/lwc/moviePoster/moviePoster.js
@@ -0,0 +1,22 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS = [
+    'Movie__c.Poster__c'
+];
+
+export default class MoviePoster extends LightningElement {
+    @api recordId;  
+    posterUrl;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    record({ error, data }) {
+        if (data) {
+            this.posterUrl = data.fields.Poster__c.value;
+            console.log("here: ", this.posterUrl);
+        } else if (error) {
+            // Handle error case
+            console.error(error);
+        }
+    }
+}
